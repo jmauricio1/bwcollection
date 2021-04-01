ed3ead6cf9025cd20633f6dfeab1267062d880cb
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2021-04-01 14:13:50 -0700

    Finishing responsiveness for multiple devics and platforms and simple refactoring of javascript code

--- public/main.js
+++ public/main.js
@@ -1,39 +1,45 @@
-window.addEventListener('scroll', (e) => {
-    const target = document.querySelectorAll('.scroll');
+window.addEventListener("scroll", (e) => {
+  const target = document.querySelectorAll(".scroll");
 
-    var currentOffset = window.pageYOffset;
+  var currentOffset = window.pageYOffset;
 
-    var w = window.innerWidth;
-    var ratio = 1;
+  var w = window.innerWidth;
+  var ratio = 1;
+  var startingPos = 0;
 
-    console.log(currentOffset);
+  //console.log(currentOffset);
 
-    if(w < 485){
-        console.log(w);
-        ratio = 0.5;
+  if (w <= 480) {
+    startingPos = 800;
+    ratio = 0.5;
+  } else if (w <= 768) {
+    startingPos = 1700;
+    ratio = 0.75;
+  } else if (w <= 1024) {
+    startingPos = 2400;
+  } else if (w <= 1440) {
+    startingPos = 3100;
+  } else {
+    startingPos = 3400;
+  }
+
+  //This for loop handles the position rate of the first 5 images on the web page
+  for (let i = 0; i < 5; i++) {
+    if (currentOffset > 100 && currentOffset < 3400) {
+      //console.log(w);
+      let pos = (currentOffset - 100) * target[i].dataset.rate;
+      target[i].style.transform =
+        "translate3d( 0px, " + pos * ratio + "px, 0px)";
     }
+  }
 
-    for(let i = 0; i < 5; i++){
-        if((currentOffset > 100) && (currentOffset < 3400)){
-            console.log(window.pageYOffset);
-            let pos = ((currentOffset) - 100) * target[i].dataset.rate;
-            target[i].style.transform = 'translate3d( 0px, ' + (pos * ratio) + 'px, 0px)';
-        }
+  //These conditional statements handle the position rate last handful of images
+  //depending on the screeen size
+  for (let i = 5; i < target.length; i++) {
+    if (currentOffset >= startingPos) {
+      let pos = (currentOffset - startingPos) * target[i].dataset.rate;
+      target[i].style.transform =
+        "translate3d( 0px, " + pos * ratio + "px, 0px)";
     }
-    for(let i = 5; i < target.length; i++){
-        if((currentOffset >= 3400)){
-            let pos = ((currentOffset) - 3400) * target[i].dataset.rate;
-            target[i].style.transform = 'translate3d( 0px, ' + (pos * ratio) + 'px, 0px)';
-        }
-    }
-
-    if(window.innerWidth < 480){
-        for(let i = 5; i < target.length; i++){
-            if((currentOffset >= 800)){
-                let pos = ((currentOffset) - 800) * target[i].dataset.rate;
-                target[i].style.transform = 'translate3d( 0px, ' + (pos * ratio) + 'px, 0px)';
-            }
-        }
-    }
-
-})
+  }
+});
--- src/components/title.js
+++ src/components/title.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
-function Title(){
-    return(
-        <section className="title-section">
-            <h1 className="page-heading">the black and white collection</h1>
-            <hr />
-        </section>
-    )
+function Title() {
+  return (
+    <section className="title-section">
+      <div className="heading-container">
+        <h1 className="page-heading">the black and white collection</h1>
+        <hr />
+      </div>
+    </section>
+  );
 }
 
 export default Title;
